2bdded4643f4a45dcf5aafa58db6c353473b6dee
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-23 20:51:53 +0300

    Update Task1.java

--- src/com/drow/hw_5/task_1/Task1.java
+++ src/com/drow/hw_5/task_1/Task1.java
@@ -1,5 +1,12 @@
 package com.drow.hw_5.task_1;
 
+/*
+многострочный
+комментарий
+очень
+удобно
+*/
+
 //        Дано:
 //        Числа от 0 до 100
 //
@@ -11,10 +18,13 @@ package com.drow.hw_5.task_1;
 public class Task1 {
     public static void main(String[] args) {
         int even = 0;
+        // uneven -> odd
         int uneven = 0;
         int sum = 0;
         boolean token = false;
 
+        // форматируй код
+       // if (i % 2 == 0)... твое решение будет работать, только на определенных наборах чисел
         for (int i = 0; i<=100; i++){
             sum = sum + i;
             if (token == false){
@@ -27,6 +37,6 @@ public class Task1 {
         }
 
         System.out.println("All sum = " + sum + "\nEven sum = " + even + "\nUneven sum = " + uneven);
-
+// пустая строка не нужна
     }
 }
