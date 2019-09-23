0520944a0adbd36978e89dcfd59f72e52590ba4d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-23 20:56:24 +0300

    Update Task3.java

--- src/com/drow/hw_5/task_3/Task3.java
+++ src/com/drow/hw_5/task_3/Task3.java
@@ -3,7 +3,7 @@ package com.drow.hw_5.task_3;
 // Выведите на экран n раз фразу "You are welcome!". Число n передать аргументом в программу
 
 import java.util.Scanner;
-
+//+
 public class Task3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
