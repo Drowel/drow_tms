bdecdef70833e7bfdb28aeadc37e6348ff7e466b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-23 20:55:43 +0300

    Update Task2.java

--- src/com/drow/hw_5/task_2/Task2.java
+++ src/com/drow/hw_5/task_2/Task2.java
@@ -13,12 +13,14 @@ public class Task2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        // 5 вынеси в отдельную переменную, myArray - плохое название для переменной
         int[] myArray = new int[5];
         for (int i = 0; i < 5; i++) {
             System.out.println("Enter a number "+ i + ": ");
             myArray[i] = in.nextInt();
         }
 
+        // слишком сложная логика. мин-макс можно найти за один проход по массиву 'myArray'
         for (int i = 4; i > 0; i--) {
             for (int j = 0; j < i ; j++){
                 if (myArray[j] > myArray[j+1]){
