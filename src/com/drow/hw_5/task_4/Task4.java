7fef4e15e266457a551b28cebc562749ca96c27f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-23 20:58:43 +0300

    Update Task4.java

--- src/com/drow/hw_5/task_4/Task4.java
+++ src/com/drow/hw_5/task_4/Task4.java
@@ -10,7 +10,12 @@ package com.drow.hw_5.task_4;
 
 public class Task4 {
     public static void main(String[] args) {
+        /*
+        я думаю можно вынести в отдельную переменную '.' и печатать ее при помощи двух циклов
+        твой вариант слишком завязан на исходной строке
+        */
         String string = "......";
+        // 6 - в отдельную переменную
         for (int i = 0; i < 6; i++){
             String subString = string.substring(i);
                 System.out.println(subString);
