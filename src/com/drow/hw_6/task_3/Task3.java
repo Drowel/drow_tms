9d4c8c02023552754bbc7db2c8d4c97881750afa
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-27 20:43:26 +0300

    Update Task3.java

--- src/com/drow/hw_6/task_3/Task3.java
+++ src/com/drow/hw_6/task_3/Task3.java
@@ -6,7 +6,9 @@ package com.drow.hw_6.task_3;
 
 import java.util.Scanner;
 
+// форматируй код
 public class Task3 {
+    // метод называется setArray() при этом ничего не принимает, лучше изменить название    
     public void setArray()throws WrongSizeException{
         Scanner in = new Scanner(System.in);
         System.out.println("Enter array size, but not bigger than 9: ");
@@ -18,11 +20,15 @@ public class Task3 {
             System.out.println(size);
         } catch (WrongSizeException e){
             System.out.println(e.getMessage());
+                
+            // такие вызовы лучше уже делать за блоком catch, а в конце try вызывать return
+            // я бы рекомендовал не использовать рекурсию, лучше цикл
             System.out.println("Please enter n again");
             this.setArray();
         }
     }
 
+        // метод main() ставь первым из методов
     public static void main(String[] args) throws WrongSizeException {
         Task3 task3 = new Task3();
         task3.setArray();
