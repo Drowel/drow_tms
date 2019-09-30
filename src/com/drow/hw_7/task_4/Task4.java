fc33636e2a1897c58d30256e112e06722c046e08
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-30 20:34:05 +0300

    Update Task4.java

--- src/com/drow/hw_7/task_4/Task4.java
+++ src/com/drow/hw_7/task_4/Task4.java
@@ -10,26 +10,30 @@ import java.io.*;
 public class Task4 {
     public static final String PATH = "src/com/drow/hw_7/task_4/Employee.dat";
 
+        // зачем тебе этот констуктор?
     public Task4() throws IOException {
     }
 
     public static void main(String[] args) throws IOException {
+            // oop -> os, oop все же имеет общепринятую расшифровку
         try (ObjectOutputStream oop = new ObjectOutputStream(new FileOutputStream(PATH));){
             Employee employee = new Employee();
             oop.writeObject(employee);
         } catch (Exception e){
-
+// лучше не оставлять catch пустым
     }
 
+            // и чтение и запись уже стоит выносить в отдельный метод
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(PATH))) {
             try {
                 Employee employee = (Employee) ois.readObject();
+                    // переопредели метод toString() и используй его
                 System.out.println("Employee " + employee.getName() + " " + employee.getAge());
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
 
         }
-
+// пустая строка не нужна
     }
 }
