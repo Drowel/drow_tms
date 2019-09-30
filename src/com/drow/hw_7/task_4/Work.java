8c2c52153802a33596454662d52467c23b6f1ab8
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-30 20:30:12 +0300

    Update Work.java

--- src/com/drow/hw_7/task_4/Work.java
+++ src/com/drow/hw_7/task_4/Work.java
@@ -3,7 +3,9 @@ package com.drow.hw_7.task_4;
 import java.io.Serializable;
 
 public class Work implements Serializable {
+    // private
     public String companyName;
+    // private
     public int stage;
 
         Work(){
