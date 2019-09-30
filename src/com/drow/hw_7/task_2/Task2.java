b72c0f58c6b3686f6b3123ecb05419233e74f24b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-30 20:26:21 +0300

    Update Task2.java

--- src/com/drow/hw_7/task_2/Task2.java
+++ src/com/drow/hw_7/task_2/Task2.java
@@ -7,20 +7,23 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 
+// форматируй код
 public class Task2 {
     public static final String PATH = "src/com/drow/hw_7/task_2/myFile.txt";
     public static void main(String[] args) {
+        // myString -> text
         String myString = "Hello there";
         try(FileWriter fos = new FileWriter(PATH)) {
             for (int i=0; i<100;i++) {              // поставил 100 что бы быстрее грузолось для таска3
                 fos.write(myString);
             }
 
+         // про флаш забыл
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+// пустая строка не нужна
     }
 }
