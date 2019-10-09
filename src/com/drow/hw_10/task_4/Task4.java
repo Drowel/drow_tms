ea11873b2cf0810914e3d4a2ab63cc3a3591c01c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-09 21:08:02 +0300

    Update Task4.java

--- src/com/drow/hw_10/task_4/Task4.java
+++ src/com/drow/hw_10/task_4/Task4.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 
 public class Task4 {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList1 = new ArrayList<>(10);
+        ArrayList<Integer> arrayList1 = new ArrayList<>(10); // 10, ты уже знаешь
         ArrayList<Integer> arrayList2 = new ArrayList<>(10);
 
         for(int i=0; i < 10; i++){
@@ -15,8 +15,10 @@ public class Task4 {
             arrayList2.add(i*2);
         }
 
+        // arrayList1.retainAll(arrayList2);
         Iterator<Integer> integerIterator = arrayList1.listIterator();
         while (integerIterator.hasNext()) {
+            // if (!arrayList2.contains(integerIterator.next())) <- ! 
             if (arrayList2.contains(integerIterator.next())==false) {
                 integerIterator.remove();
             }
@@ -26,5 +28,6 @@ public class Task4 {
         for(int i=0; i < arrayList1.size(); i++){
             System.out.println(arrayList1.get(i));
         }
+        // System.out.println(arrayList1);
     }
 }
