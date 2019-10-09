a637df051b32e12f924f3b7b9ee2eaa939ca647d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-09 21:05:57 +0300

    Update Task3.java

--- src/com/drow/hw_10/task_3/Task3.java
+++ src/com/drow/hw_10/task_3/Task3.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 
 public class Task3 {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList1 = new ArrayList<>(10);
+        ArrayList<Integer> arrayList1 = new ArrayList<>(10); // 10 по умолчанию, можно не передавать в конструктор
         ArrayList<Integer> arrayList2 = new ArrayList<>(10);
 
         for(int i=0; i < 10; i++){
@@ -15,7 +15,9 @@ public class Task3 {
             arrayList2.add(i*2);
         }
 
-      /*  System.out.println("Array 1: ");
+        // не оставляй закомментированный код в готовой версии программы
+      /* 
+      System.out.println("Array 1: ");
         for(int i=0; i < 10; i++){
             System.out.println(arrayList1.get(i));
         }
@@ -23,8 +25,10 @@ public class Task3 {
         System.out.println("Array 2: ");
         for(int i=0; i < 10; i++){
             System.out.println(arrayList2.get(i));
-        }*/
+        }
+        */
 
+        // arrayList1.removeAll(arrayList2)
         Iterator<Integer> integerIterator = arrayList1.listIterator();
         while (integerIterator.hasNext()) {
             if (arrayList2.contains(integerIterator.next())) {
@@ -32,9 +36,10 @@ public class Task3 {
             }
         }
 
-        System.out.println("Array : ");
+        System.out.println("Array : "); // <- array = массив
         for(int i=0; i < arrayList1.size(); i++){
             System.out.println(arrayList1.get(i));
         }
+        // System.out.println(arrayList1);
     }
 }
