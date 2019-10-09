f81466e24f1fe7ef08d35ec9085cf8fe19fe1b5e
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-09 20:55:49 +0300

    Update Task1.java

--- src/com/drow/hw_10/task_1/Task1.java
+++ src/com/drow/hw_10/task_1/Task1.java
@@ -8,9 +8,11 @@ import java.util.Scanner;
         Есть список имен (коллекция). Пользователь вводит с консоли свое имя, а программа проверяет его наличие в списке,
         после чего говорит может он пройти или нет
 */
+
+//не забывай форматировать код
 public class Task1 {
     public static void main(String[] args){
-        ArrayList<Person> personList = new ArrayList<>(10);
+        ArrayList<Person> personList = new ArrayList<>(10); // 10 - это число по умолчанию, его можно не передавать
         Person ann = new Person("Ann", 22);
         Person john = new Person("John", 24);
         Person jane = new Person("Jane", 18);
@@ -28,8 +30,8 @@ public class Task1 {
             for (int i = 0; i<personList.size(); i++) {
                if(personList.get(i).getName().equals(consoleIn)){
                    System.out.println("You are welcome!");
-                   break;
-               } if(i == personList.size()-1){
+                   break; //можно здесь поставить return, тогда if не нужен
+               } if(i == personList.size()-1){ //не забывай форматировать код
                 System.out.println("You a not in list");
                }
             }
