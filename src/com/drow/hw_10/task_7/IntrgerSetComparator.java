2774ccb78144e482d4534c5633544882759be5cf
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-09 21:13:51 +0300

    Update IntrgerSetComparator.java

--- src/com/drow/hw_10/task_7/IntrgerSetComparator.java
+++ src/com/drow/hw_10/task_7/IntrgerSetComparator.java
@@ -2,6 +2,7 @@ package com.drow.hw_10.task_7;
 
 import java.util.Comparator;
 
+// название класса с опечаткой, Set в названии лишнее
 public class IntrgerSetComparator implements Comparator<Integer> {
     @Override
     public int compare(Integer o1, Integer o2) {
