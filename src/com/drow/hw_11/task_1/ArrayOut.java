52245df7e3b97cfb9b2d0f25e90e71ccdae68fd9
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-14 21:49:12 +0300

    Update ArrayOut.java

--- src/com/drow/hw_11/task_1/ArrayOut.java
+++ src/com/drow/hw_11/task_1/ArrayOut.java
@@ -1,7 +1,7 @@
 package com.drow.hw_11.task_1;
 
 public class ArrayOut extends Thread {
-    public int[] myArray = new int[11];
+    public int[] myArray = new int[11]; // лучше через конструктор передавать
 
     ArrayOut() {
         for (int i = 0; i < 11; i++) {
@@ -10,6 +10,7 @@ public class ArrayOut extends Thread {
     }
 
     public void run() {
+        // 11 - через конструктор
         for (int i = 0; i < 11; i++) {
             System.out.println(myArray[i]);
             try {
