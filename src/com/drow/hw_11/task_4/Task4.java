647367df46737787acff754f1171a6680dd3511c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-14 21:51:53 +0300

    Update Task4.java

--- src/com/drow/hw_11/task_4/Task4.java
+++ src/com/drow/hw_11/task_4/Task4.java
@@ -3,6 +3,7 @@ package com.drow.hw_11.task_4;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+// +
 public class Task4 {
     public static void main(String[] args) {
         LinkedList<String> words = new LinkedList<>();  // change to LinkedList<>
